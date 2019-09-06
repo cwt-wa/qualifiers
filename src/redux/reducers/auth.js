d47069fef3e971bdc321b625c06be49a77595ee1
Zemke <florian@zemke.io>
2019-09-06 22:05:16 +0200

    Store parsed token payload

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,7 +1,7 @@
 import {AUTH_LOGIN, AUTH_LOGOUT} from "../actionTypes";
 
 export default function (state = null, action) {
-  if (action.type === AUTH_LOGIN) return action.token;
+  if (action.type === AUTH_LOGIN) return JSON.parse(atob(action.token.split('.')[1])).context.user;
   else if (action.type === AUTH_LOGOUT) return null;
   else return null;
 };
