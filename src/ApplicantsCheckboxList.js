91efc864ab30ba89302b2e3cd5e1a27d82b5e789
Zemke <florian@zemke.io>
2019-09-07 00:40:02 +0200

    Dedicated checkbox list component

--- src/ApplicantsCheckboxList.js
+++ src/ApplicantsCheckboxList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Fetch from './fetch';
+import CheckboxList from "./CheckboxList";
 
 const toastr = require('toastr');
 
@@ -26,19 +27,6 @@ export default class ApplicantsCheckboxList extends React.Component {
         this.props.submit(this.state.drawnUsers);
     };
 
-    toggleCheck = (add, userId) => {
-        this.setState(state => {
-            if (add) state.drawnUsers.push(state.applicants.find(a => a.applicant.id === userId).applicant);
-            if (!add) state.drawnUsers.splice(state.drawnUsers.findIndex(dA => dA.id === userId), 1);
-
-            return {drawnUsers: state.drawnUsers};
-        });
-    };
-
-    isDrawn = (userId) => {
-        return this.state.drawnUsers.find(dA => dA.id === userId) != null;
-    };
-
     render() {
         if (!this.state.applicants.length) {
             return <p>There are currently no applicants.</p>
@@ -48,12 +36,10 @@ export default class ApplicantsCheckboxList extends React.Component {
             <form onSubmit={this.submit}>
                 <p>Select applicants to be drawn into qualifiers.</p>
 
-                {this.state.applicants.map(a =>
-                    <div key={a.id}>
-                        <input name="applicant" id={'applicant' + a.id} type="checkbox"
-                               checked={this.isDrawn(a.applicant.id)} onChange={e => this.toggleCheck(e.target.checked, a.applicant.id)}/>
-                        <label className="label-inline" htmlFor={'applicant' + a.id}>{a.applicant.username} {this.isDrawn(a.applicant.id)}</label>
-                    </div>)}
+                <CheckboxList
+                    elems={this.state.applicants.map(a => a.applicant)}
+                    onSelection={drawnUsers => this.setState({drawnUsers})}
+                    displayKey="username"/>
 
                 <input type="submit" value="Submit"/>
             </form>);
---
+++ src/CheckboxList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default class CheckboxList extends React.Component {
+
+  state = {selected: []};
+
+  toggle = elem => {
+    this.setState(state => {
+      if (state.selected.indexOf(elem) !== -1) state.selected.splice(this.props.selected.indexOf(elem), 1);
+      else state.selected.push(elem);
+      return {selected: state.selected};
+    }, () => this.props.onSelection(this.state.selected));
+  };
+
+  isSelected = (elem) => {
+    return this.state.selected.indexOf(elem) !== -1;
+  };
+
+  render() {
+    return (<>
+      {this.props.elems.map((elem, idx) =>
+          <div key={idx}>
+            <input name="applicant" id={'elem' + idx} type="checkbox"
+                   checked={this.isSelected(elem)} onChange={() => this.toggle(elem)}/>
+            <label className="label-inline" htmlFor={'elem' + idx}>{elem[this.props.displayKey]}</label>
+          </div>)
+      }
+    </>)
+  }
+}
