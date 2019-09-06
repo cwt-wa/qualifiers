2eb17ace49de17e28546b3c230fd7880829a5e2f
Zemke <florian@zemke.io>
2019-09-06 22:08:25 +0200

    Remove error boundary

--- src/DrawOpponents.js
+++ src/DrawOpponents.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ApplicantsCheckboxList from "./ApplicantsCheckboxList";
-import ErrorBoundary from "./ErrorBoundary";
 
 export default class DrawOpponents extends React.Component {
 
     render() {
-        return <ErrorBoundary><ApplicantsCheckboxList/></ErrorBoundary>
+        return <ApplicantsCheckboxList/>
     }
 };
