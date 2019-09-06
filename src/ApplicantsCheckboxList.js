b03811fa9fe4e7eba031883c718ac3d1224398bf
Zemke <florian@zemke.io>
2019-09-06 22:18:42 +0200

    Simplify render method

--- src/ApplicantsCheckboxList.js
+++ src/ApplicantsCheckboxList.js
@@ -11,27 +11,19 @@ export default class ApplicantsCheckboxList extends React.Component {
     }
 
     render() {
-        if (this.state.errorLoadingUsers) {
-            return (
-                <p className="error">
-                    Could not load applicants.
-                </p>
-            );
-        } else {
-            if (!this.state.applicants.length) {
-                return <p>There are no applicants currently.</p>
-            } else {
-                return (
-                    <div>
-                        {this.state.applicants.map(a =>
-                            <div key={a.id}>
-                                <label>
-                                    <input name="applicant" type="checkbox"/>{a.applicant.username}
-                                </label>
-                            </div>)}
-                    </div>
-                );
-            }
+        if (!this.state.applicants.length) {
+            return <p>There are currently no applicants.</p>
         }
+
+        return (
+            <div>
+                {this.state.applicants.map(a =>
+                    <div key={a.id}>
+                        <label>
+                            <input name="applicant" type="checkbox"/>{a.applicant.username}
+                        </label>
+                    </div>)}
+            </div>
+        );
     }
 };
